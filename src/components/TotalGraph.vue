025c2e60af1d9424ac3280e0c6c658056b32db58
totorotail <jminkyun@gmail.com>
2025-03-17 17:33:48 +0900

    style: TotalGraph 테두리 제거

--- src/components/TotalGraph.vue
+++ src/components/TotalGraph.vue
@@ -102,8 +102,8 @@ interface ExamResult {
         </div>
 
         <div class="overflow-y-scroll max-h-[500px]">
-            <div v-for="graph in filteredGraphs" :key="graph.id" class="border p-4 mb-2 rounded-lg">
-                <div class="flex items-center space-x-2">
+            <div v-for="graph in filteredGraphs" :key="graph.id" class="p-4 mb-2">
+                <div class="flex items-center space-x-2">w
                     <span class="px-2 py-1 rounded text-xs font-bold" :class="[
                         testStore.testCategories.find(test => test.id === graph.id)?.bg || 'bg-gray-200',
                         testStore.testCategories.find(test => test.id === graph.id)?.color || 'text-gray-800'
