0778deefd3e3186523ef859a5648849205898654
totorotail <jminkyun@gmail.com>
2025-04-17 19:38:15 +0900

    feat: 시작 시 인증 상태 초기화

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,16 @@ import './style.css'
 import App from './App.vue'
 import router from './router/router.ts';
 import VueGoogleCharts from 'vue-google-charts';
+import AuthService from './services/AuthService'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faUserPlus, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faUserPlus, faMagnifyingGlass);
+
+// 앱 시작 시 인증 상태 초기화
+AuthService.initAuth();
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
