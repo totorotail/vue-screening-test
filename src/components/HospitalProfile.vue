553b6097faf15f6cee82dfa339229d542a91b755
totorotail <jminkyun@gmail.com>
2025-04-18 00:58:05 +0900

    refactor: HospitalProfile.vue

---
+++ src/components/HospitalProfile.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps<{
+  email: string;
+  hospitalName: string;
+  location: string;
+  plan: string;
+}>();
+
+defineEmits(['open-edit']);
+</script>
+
+<template>
+    <div class="w-1/3 bg-white shadow-lg rounded-lg p-6">
+      <div class="grid grid-cols-[80px_1fr] gap-1">
+        <div class="font-semibold">이메일</div>
+        <div>{{ email }}</div>
+        <div class="font-semibold">병원이름</div>
+        <div>{{ hospitalName }}</div>
+        <div class="font-semibold">지역</div>
+        <div>{{ location }}</div>
+        <div class="font-semibold">플랜</div>
+        <div>{{ plan }}</div>
+      </div>
+      <div class="mt-4">
+        <button @click="$emit('open-edit')">
+          <img src="src/assets/setting-icon.png" class="w-6 h-6" alt="설정" />
+        </button>
+      </div>
+    </div>
+  </template>
