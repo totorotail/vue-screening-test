c7ebe54a47addd03e43cc1c2ebcf5707518a5459
totorotail <jminkyun@gmail.com>
2025-04-25 08:58:29 +0900

    chore: add Vite alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,6 +9,12 @@ export default defineConfig({
         vue(),
         tailwindcss(),
     ],
+    resolve: {
+        alias: {
+          '@': path.resolve(__dirname, 'src'),
+          'src': path.resolve(__dirname, 'src')
+        }
+      },
     server: {
         proxy: {
             // 개발 환경에서만 적용되는 프록시 설정
